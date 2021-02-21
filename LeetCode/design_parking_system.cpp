fa4670563736053941e60d47ab69d5d4f493c224
João Daniel Rufino <joaodaniel0405@gmail.com>
2021-02-21 12:55:44 -0300

    Create design_parking_system.cpp

---
+++ LeetCode/design_parking_system.cpp
@@ -0,0 +1,20 @@
+class ParkingSystem {
+public:
+    int arr[3];
+    
+    ParkingSystem(int big, int medium, int small) {
+        arr[0] = big;
+        arr[1] = medium;
+        arr[2] = small;
+    }
+    
+    bool addCar(int carType) {
+        return arr[carType-1]-- > 0;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */

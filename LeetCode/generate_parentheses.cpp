159933d205a5861640dc1f7fb4a454eac23d8a5b
João Daniel Rufino <joaodaniel0405@gmail.com>
2021-07-01 12:53:02 -0300

    Update generate_parentheses.cpp

--- LeetCode/generate_parentheses.cpp
+++ LeetCode/generate_parentheses.cpp
@@ -20,3 +20,27 @@ public:
         return vet;
     }
 };
+
+/* Or this
+class Solution {
+public:
+    vector<string> res;
+    
+    void solve(string str, int open, int close) {
+        if(!open && !close) {
+            res.push_back(str);
+        }
+        
+        if(open)
+            solve(str + '(', open-1, close);
+        if(close > open)
+            solve(str + ')', open, close-1);
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        solve("", n, n);
+        
+        return res;
+    }
+};
+*/

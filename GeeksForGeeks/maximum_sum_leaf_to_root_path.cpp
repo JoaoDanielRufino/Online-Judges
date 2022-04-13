ec836d4002abac43d2c62d2919240d1e09848568
João Daniel Rufino <joaodaniel0405@gmail.com>
2022-04-13 20:26:58 -0300

    Create maximum_sum_leaf_to_root_path.cpp

---
+++ GeeksForGeeks/maximum_sum_leaf_to_root_path.cpp
@@ -0,0 +1,26 @@
+class Solution{
+    public:
+    int ans = INT_MIN;
+    
+    void solve(Node* root, int currSum) {
+        if(!root)
+            return;
+            
+        if(!root->left && !root->right) {
+            ans = max(ans, currSum + root->data);
+            return;
+        }
+        
+        if(root->left)
+            solve(root->left, currSum + root->data);
+        if(root->right)
+            solve(root->right, currSum + root->data);
+    }
+    
+    int maxPathSum(Node* root)
+    {
+        solve(root, 0);
+        
+        return ans;
+    }
+};

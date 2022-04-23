30a89a615c6a2ebb46ab16c59a569c27b7355985
João Daniel Rufino <joaodaniel0405@gmail.com>
2022-04-23 01:02:03 -0300

    Update contains_close_nums.cpp

--- CodeSignal/HashTables/contains_close_nums.cpp
+++ CodeSignal/HashTables/contains_close_nums.cpp
@@ -1,20 +1,10 @@
 bool solution(vector<int> nums, int k) {
-    map<int, vector<int>> mp;
+    unordered_map<int, int> mp;
     
     for(int i = 0; i < nums.size(); i++) {
-        mp[nums[i]].push_back(i);
-    }
-    
-    for(auto it = mp.begin(); it != mp.end(); it++) {
-        vector<int> positions = it->second;
-        for(int i = 0; i < positions.size()-1; i++) {
-            for(int j = i+1; j < positions.size(); j++) {
-                if(abs(positions[i] - positions[j]) <= k)
-                    return true;
-                else
-                    break;
-            }
-        }
+        if(mp.find(nums[i]) != mp.end() && i - mp[nums[i]] <= k)
+            return true;
+        mp[nums[i]] = i;
     }
     
     return false;

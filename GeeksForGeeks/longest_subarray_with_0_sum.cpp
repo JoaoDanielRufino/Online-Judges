125546fb9fb611dc7dd9e8ba198478d5198a7f45
João Daniel Rufino <joaodaniel0405@gmail.com>
2022-04-25 12:59:03 -0300

    Create longest_subarray_with_0_sum.cpp

---
+++ GeeksForGeeks/longest_subarray_with_0_sum.cpp
@@ -0,0 +1,25 @@
+// https://www.youtube.com/watch?v=xmguZ6GbatA
+
+class Solution{
+    public:
+    int maxLen(vector<int>&A, int n)
+    {   
+        int ans = 0;
+        unordered_map<int, int> mp;
+        
+        int curr = 0;
+        for(int i = 0; i < A.size(); i++) {
+            curr += A[i];
+            
+            if(!curr)
+                ans = i+1;
+            
+            if(mp.find(curr) != mp.end())
+                ans = max(ans, i - mp[curr]);
+            else
+                mp[curr] = i;
+        }
+        
+        return ans;
+    }
+};

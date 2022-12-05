1adfc53283abfdcc7437a1117c0524b7fb24dab8
João Daniel Rufino <joaodaniel0405@gmail.com>
2022-12-05 15:42:57 -0300

    Update combinations.cpp

--- LeetCode/combinations.cpp
+++ LeetCode/combinations.cpp
@@ -21,3 +21,31 @@ public:
         return ans;
     }
 };
+
+// Or
+class Solution {
+public:
+    vector<vector<int>> solve(int n, int k, int start, vector<int> curr) {
+        vector<vector<int>> ans;
+
+        if(curr.size() == k) {
+            ans.push_back(curr);
+            return ans;
+        }
+
+        for(int i = start; i <= n; i++) {
+            curr.push_back(i);
+            vector<vector<int>> aux = solve(n, k, i+1, curr);
+            for(vector<int> vet : aux) {
+                ans.push_back(vet);
+            }
+            curr.pop_back();
+        }
+
+        return ans;
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        return solve(n, k, 1, vector<int> ());
+    }
+};

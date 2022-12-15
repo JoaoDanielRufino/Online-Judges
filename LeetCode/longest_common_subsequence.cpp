69b99005b07dbefd2a5fdb6291ff3d6b5ec1fd78
João Daniel Rufino <joaodaniel0405@gmail.com>
2022-12-15 15:09:53 -0300

    Update longest_common_subsequence.cpp

--- LeetCode/longest_common_subsequence.cpp
+++ LeetCode/longest_common_subsequence.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+    int solve(string& a, string& b, int i, int j, vector<vector<int>>& dp) {
+        if(i == a.size() || j == b.size())
+            return 0;
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        if(a[i] == b[j])
+            return dp[i][j] = 1 + solve(a, b, i + 1, j + 1, dp);
+
+        return dp[i][j] = max(solve(a, b, i + 1, j, dp), solve(a, b, i, j + 1, dp));
+    }
+
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>> dp(text1.size(), vector<int> (text2.size(), -1));
+
+        return solve(text1, text2, 0, 0, dp);
+    }
+};
+
+// Or
+class Solution {
+public:
     int longestCommonSubsequence(string text1, string text2) {
         int dp[text1.size()+1][text2.size()+1];
         

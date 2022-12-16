c63af8252bc69566a13c8376a8b638c20a8d4ea3
João Daniel Rufino <joaodaniel0405@gmail.com>
2022-12-16 15:17:32 -0300

    Update implement_queue_using_stacks.cpp

--- LeetCode/implement_queue_using_stacks.cpp
+++ LeetCode/implement_queue_using_stacks.cpp
@@ -1,43 +1,35 @@
 class MyQueue {
 public:
-    stack<int> s1, s2;
-    /** Initialize your data structure here. */
+    stack<int> st;
+
     MyQueue() {
         
     }
     
-    /** Push element x to the back of queue. */
     void push(int x) {
-        if(s1.empty())
-            s1.push(x);
-        else {
-            while(!s1.empty()) {
-                s2.push(s1.top());
-                s1.pop();
-            }
-            s1.push(x);
-            while(!s2.empty()) {
-                s1.push(s2.top());
-                s2.pop();
-            }
+        if(st.empty()){ 
+            st.push(x);
+            return;
         }
+
+        int top = st.top();
+        st.pop();
+        push(x);
+        st.push(top);
     }
     
-    /** Removes the element from in front of queue and returns that element. */
     int pop() {
-        int top = s1.top();
-        s1.pop();
-        return top;
+        int x = st.top();
+        st.pop();
+        return x;
     }
     
-    /** Get the front element. */
     int peek() {
-        return s1.top();
+        return st.top();
     }
     
-    /** Returns whether the queue is empty. */
     bool empty() {
-        return s1.empty();
+        return st.empty();
     }
 };
 

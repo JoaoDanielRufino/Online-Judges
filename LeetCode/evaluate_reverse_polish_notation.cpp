8cab7700cb3067198991740d3e266e4bbd0fbffe
João Daniel Rufino <joaodaniel0405@gmail.com>
2022-12-17 10:43:23 -0300

    Update evaluate_reverse_polish_notation.cpp

--- LeetCode/evaluate_reverse_polish_notation.cpp
+++ LeetCode/evaluate_reverse_polish_notation.cpp
@@ -1,27 +1,39 @@
 class Solution {
-public:    
-    int evalRPN(vector<string>& tokens) {
-        stack<int> st;
+public:
+    bool isNumber(string str) {
+        if(isdigit(str[0]))
+            return true;
+
+        if(str[0] == '-' && str.size() > 1)
+            return true;
         
+        return false;
+    }
+
+    int evalRPN(vector<string>& tokens) {
+        stack<long long int> st;
+
         for(string token : tokens) {
-            if(isdigit(token[0]) || (token[0] == '-' && token.size() > 1))
+            if(isNumber(token))
                 st.push(stoi(token));
             else {
                 int b = st.top();
                 st.pop();
+
                 int a = st.top();
                 st.pop();
+
                 if(token == "+")
                     st.push(a + b);
                 else if(token == "-")
                     st.push(a - b);
                 else if(token == "*")
-                    st.push(a * b);
+                    st.push((long long) a * b);
                 else
                     st.push(a / b);
             }
         }
-        
+
         return st.top();
     }
 };

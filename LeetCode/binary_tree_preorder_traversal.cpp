d75d4c585a97d05b92d2371109dfff50072748fb
João Daniel Rufino <joaodaniel0405@gmail.com>
2023-01-09 10:44:19 -0300

    Update binary_tree_preorder_traversal.cpp

--- LeetCode/binary_tree_preorder_traversal.cpp
+++ LeetCode/binary_tree_preorder_traversal.cpp
@@ -27,3 +27,24 @@ public:
         return res;
     }
 };
+
+// Recursive
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+
+        if(!root)
+            return ans;
+
+        ans.push_back(root->val);
+
+        vector<int> left = preorderTraversal(root->left);
+        vector<int> right = preorderTraversal(root->right);
+
+        ans.insert(ans.end(), left.begin(), left.end());
+        ans.insert(ans.end(), right.begin(), right.end());
+
+        return ans;
+    }
+};

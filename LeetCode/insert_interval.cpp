a0ce49c271a0a47cdd2eb41e865d1651d37af94d
João Daniel Rufino <joaodaniel0405@gmail.com>
2023-01-16 11:12:02 -0300

    Update insert_interval.cpp

--- LeetCode/insert_interval.cpp
+++ LeetCode/insert_interval.cpp
@@ -65,3 +65,28 @@ public:
         return res;
     }
 };
+
+// Solution 3
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+
+        int i = 0;
+        while(i < intervals.size() && intervals[i][1] < newInterval[0]) {
+            ans.push_back(intervals[i]);
+            i++;
+        }
+
+        while(i < intervals.size() && intervals[i][0] <= newInterval[1]) {
+            newInterval = { min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1]) };
+            i++;
+        }
+
+        ans.push_back(newInterval);
+
+        ans.insert(ans.end(), intervals.begin() + i, intervals.end());
+
+        return ans;
+    }
+};

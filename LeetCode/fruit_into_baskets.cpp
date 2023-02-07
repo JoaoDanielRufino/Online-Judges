753cff059f0120ebaccf16815d5be35a25426c72
João Daniel Rufino <joaodaniel0405@gmail.com>
2023-02-07 11:02:21 -0300

    Update fruit_into_baskets.cpp

--- LeetCode/fruit_into_baskets.cpp
+++ LeetCode/fruit_into_baskets.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        map<int, int> mp;
-        
-        int l = 0, r = 0, res = INT_MIN;
+        unordered_map<int, int> mp;
+        int ans = 0;
+
+        int l = 0, r = 0;
         while(r < fruits.size()) {
             mp[fruits[r]]++;
-            
-            if(mp.size() <= 2) {
-                int size = 0;
-                for(auto it : mp) {
-                    size += it.second;
-                }
-                res = max(res, size);
-            } else {
-                while(mp.size() > 2) {
-                    mp[fruits[l]]--;
-                    
-                    if(!mp[fruits[l]])
-                        mp.erase(fruits[l]);
-                
-                    l++;
-                }
+
+            while(mp.size() > 2) {
+                mp[fruits[l]]--;
+                if(!mp[fruits[l]])
+                    mp.erase(fruits[l]);
+                l++;
             }
-            
+
+            ans = max(ans, r - l + 1);
+
             r++;
         }
-        
-        return res;
+
+        return ans;
     }
 };

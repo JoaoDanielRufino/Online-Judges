e404b654199b366312eef46d7da417cfe6c15bc5
João Daniel Rufino <joaodaniel0405@gmail.com>
2023-02-26 11:02:00 -0300

    Update edit_distance.cpp

--- LeetCode/edit_distance.cpp
+++ LeetCode/edit_distance.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    int dp[1000][1000];
-    
-    int solve(string s1, string s2, int i, int j) {
-        if(!i)
-            return j;
-        
-        if(!j)
-            return i;
-        
+    int solve(string a, string b, int i, int j, vector<vector<int>>& dp) {
+        if(i == a.size())
+            return b.size() - j;
+
+        if(j == b.size())
+            return a.size() - i;
+
         if(dp[i][j] != -1)
             return dp[i][j];
-        
-        if(s1[i-1] == s2[j-1])
-            return dp[i][j] = solve(s1, s2, i-1, j-1);
-        
-        int aux = solve(s1, s2, i, j-1);
-        return dp[i][j] = 1 + min(aux, min(solve(s1, s2, i-1, j), solve(s1, s2, i-1, j-1)));
+
+        if(a[i] != b[j])
+            return dp[i][j] = 1 + min({solve(a, b, i + 1, j, dp), solve(a, b, i, j + 1, dp), solve(a, b, i + 1, j + 1, dp)}); // remove, insert or replace
+
+        return dp[i][j] = solve(a, b, i + 1, j + 1, dp); // do nothing 
     }
-    
+
     int minDistance(string word1, string word2) {
-        for(int i = 0; i<= word1.size(); i++) {
-            for(int j = 0; j <= word2.size(); j++) {
-                dp[i][j] = -1;
-            }
-        }
-        
-        return solve(word1, word2, word1.size(), word2.size());
+        vector<vector<int>> dp(word1.size(), vector<int> (word2.size(), -1));
+
+        return solve(word1, word2, 0, 0, dp);
     }
 };

2544e92726d22cc915d4f6840d2eccb62a3688e7
João Daniel Rufino <joaodaniel0405@gmail.com>
2023-04-24 12:35:42 -0300

    Create binary_search.cpp

---
+++ LeetCode/binary_search.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+
+        while(l <= r) {
+            int mid = l + (r - l)/2;
+
+            if(nums[mid] == target)
+                return mid;
+            if(nums[mid] > target)
+                r = mid - 1;
+            else
+                l = mid + 1;
+        }
+
+        return -1;
+    }
+};

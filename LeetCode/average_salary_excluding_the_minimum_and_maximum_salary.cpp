8e98019604dcc4d217905762fe549dbf2b27532b
João Daniel Rufino <joaodaniel0405@gmail.com>
2023-05-02 12:35:10 -0300

    Create average_salary_excluding_the_minimum_and_maximum_salary.cpp

---
+++ LeetCode/average_salary_excluding_the_minimum_and_maximum_salary.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        double ans = 0;
+
+        sort(salary.begin(), salary.end());
+
+        for(int i = 1; i < salary.size() - 1; i++)
+            ans += salary[i];
+
+        return ans / (salary.size() - 2);
+    }
+};

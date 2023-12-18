55ec3f50328686e1375c150b614d804173b7c9b6
João Daniel Rufino <joaodaniel0405@gmail.com>
2023-12-18 11:40:36 -0300

    Create sort_array_by_increasing_frequency.go

---
+++ LeetCode/sort_array_by_increasing_frequency.go
@@ -0,0 +1,17 @@
+func frequencySort(nums []int) []int {
+    count := map[int]int{}
+
+    for _, num := range nums {
+        count[num]++
+    }
+
+    sort.Slice(nums, func(i, j int) bool {
+        if count[nums[i]] == count[nums[j]] {
+            return nums[i] > nums[j]
+        }
+
+        return count[nums[i]] < count[nums[j]]
+    })
+
+    return nums
+}

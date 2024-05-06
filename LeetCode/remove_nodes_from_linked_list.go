9a5f75d85a5c9ac2294a0263061f288817a369e1
João Daniel Rufino <joaodaniel0405@gmail.com>
2024-05-06 17:22:18 -0300

    Create remove_nodes_from_linked_list.go

---
+++ LeetCode/remove_nodes_from_linked_list.go
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func max(a, b int) int {
+    if a < b {
+        return b
+    }
+
+    return a
+}
+
+func solve(node *ListNode, currMax *int) {
+    if node == nil {
+        return
+    }
+
+    solve(node.Next, currMax)
+
+    if node.Val < *currMax {
+        node.Val = node.Next.Val
+        node.Next = node.Next.Next
+    } 
+
+    *currMax = max(*currMax, node.Val)
+}
+
+func removeNodes(head *ListNode) *ListNode {
+    currMax := -1
+
+    solve(head, &currMax)
+
+    return head
+}

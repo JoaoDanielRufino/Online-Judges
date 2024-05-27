a29b3395771e985c35df9d36911520d323976d62
João Daniel Rufino <joaodaniel0405@gmail.com>
2024-05-27 12:02:44 -0300

    Update h_index.go

--- LeetCode/h_index.go
+++ LeetCode/h_index.go
@@ -10,3 +10,41 @@ func hIndex(citations []int) int {
 
     return 0
 }
+
+// binary search
+func getCitations(citations []int, h int) int {
+    count := 0
+
+    for _, citation := range citations {
+        if citation >= h {
+            count++
+        }
+    }
+
+    return count
+}
+
+func hIndex(citations []int) int {
+    maxVal := 0
+
+    for _, citation := range citations {
+        if maxVal < citation {
+            maxVal = citation
+        }
+    }
+
+    h, l, r := 0, 0, maxVal
+    for l <= r {
+        mid := (l + r) / 2
+        count := getCitations(citations, mid)
+
+        if count >= mid {
+            h = mid
+            l = mid + 1
+        } else {
+            r = mid - 1
+        }
+    }
+
+    return h
+}

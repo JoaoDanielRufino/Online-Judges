ac8374601a35574c8c3cf3443ae5495f3f01dc63
João Daniel Rufino <joaodaniel0405@gmail.com>
2024-06-04 19:34:04 -0300

    Create longest_palindrome.go

---
+++ LeetCode/longest_palindrome.go
@@ -0,0 +1,23 @@
+func longestPalindrome(s string) int {
+    freq := map[byte]int{}
+
+    for i := range s {
+        freq[s[i]]++
+    }
+
+    ans, singleOddAdded := 0, false
+    for _, val := range freq {
+        if val % 2 == 0 {
+            ans += val
+        } else {
+            if !singleOddAdded {
+                ans += val
+                singleOddAdded = true
+            } else {
+                ans += val - 1
+            }
+        }
+    }
+
+    return ans
+}

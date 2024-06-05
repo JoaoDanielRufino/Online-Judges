73de7f8c1bf227b1ca796bd8861f53f0d2f05fbf
João Daniel Rufino <joaodaniel0405@gmail.com>
2024-06-04 21:57:15 -0300

    Create longest_consecutive_sequence.go

---
+++ LeetCode/longest_consecutive_sequence.go
@@ -0,0 +1,28 @@
+func max(a, b int) int {
+    if a < b {
+        return b
+    }
+
+    return a
+}
+
+func longestConsecutive(nums []int) int {
+    seen := map[int]bool{}
+
+    for _, num := range nums {
+        seen[num] = true
+    }
+
+    ans := 0
+    for _, num := range nums {
+        if !seen[num-1] {
+            size := 1
+            for seen[num + size] {
+                size++
+            }
+            ans = max(ans, size)
+        }
+    }
+
+    return ans
+}

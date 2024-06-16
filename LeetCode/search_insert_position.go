9e5638b5214e49f1bc6c587c2cc9e0c6f9b95ff4
João Daniel Rufino <joaodaniel0405@gmail.com>
2024-06-16 13:34:46 -0300

    Create search_insert_position.go

---
+++ LeetCode/search_insert_position.go
@@ -0,0 +1,19 @@
+func searchInsert(nums []int, target int) int {
+    l, r := 0, len(nums)-1
+
+    for l <= r {
+        mid := (l + r) / 2
+
+        if nums[mid] == target {
+            return mid
+        }
+
+        if nums[mid] < target {
+            l = mid + 1
+        } else {
+            r = mid - 1
+        }
+    }
+
+    return l
+}

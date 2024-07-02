554648da20f824004ef7e65e717ea1363c7b23ea
João Daniel Rufino <joaodaniel0405@gmail.com>
2024-07-02 19:02:34 -0300

    Create binary_search_tree_to_greater_sum_tree.go

---
+++ LeetCode/binary_search_tree_to_greater_sum_tree.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func solve(root *TreeNode, currSum *int) *TreeNode {
+    if root == nil {
+        return nil
+    }
+
+    root.Right = solve(root.Right, currSum) 
+
+    *currSum += root.Val
+    root.Val = *currSum
+
+    root.Left = solve(root.Left, currSum)
+
+    return root
+}
+
+func bstToGst(root *TreeNode) *TreeNode {
+    currSum := 0
+    return solve(root, &currSum)
+}

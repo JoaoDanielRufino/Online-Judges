25ad3a97041a3d6ff4eff208869b91bfbe25c438
João Daniel Rufino <joaodaniel0405@gmail.com>
2024-07-03 16:48:01 -0300

    Create minimum_difference_between_largest_and_smallest_value_in_three_moves.go

---
+++ LeetCode/minimum_difference_between_largest_and_smallest_value_in_three_moves.go
@@ -0,0 +1,24 @@
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+
+    return b
+}
+
+func minDifference(nums []int) int {
+    if len(nums) <= 4 {
+        return 0
+    }
+
+    sort.Ints(nums)
+
+    ans, last := math.MaxInt, len(nums)-1
+
+    ans = min(ans, nums[last-3] - nums[0])
+    ans = min(ans, nums[last-2] - nums[1])
+    ans = min(ans, nums[last-1] - nums[2])
+    ans = min(ans, nums[last] - nums[3])
+
+    return ans
+}

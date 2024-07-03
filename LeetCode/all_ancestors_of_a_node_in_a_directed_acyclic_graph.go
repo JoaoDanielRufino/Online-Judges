60071d518cf5310100a21d39ff59410f2849c1b0
João Daniel Rufino <joaodaniel0405@gmail.com>
2024-07-02 21:27:20 -0300

    Create all_ancestors_of_a_node_in_a_directed_acyclic_graph.go

---
+++ LeetCode/all_ancestors_of_a_node_in_a_directed_acyclic_graph.go
@@ -0,0 +1,27 @@
+func dfs(graph map[int][]int, u, ancestor int, seen []bool, ans [][]int) {
+    seen[u] = true
+
+    for _, adj := range graph[u] {
+        if !seen[adj] {
+            ans[adj] = append(ans[adj], ancestor)
+            dfs(graph, adj, ancestor, seen, ans)
+        }
+    }
+}
+
+func getAncestors(n int, edges [][]int) [][]int {
+    graph := map[int][]int{}
+
+    for _, edge := range edges {
+        u, v := edge[0], edge[1]
+        graph[u] = append(graph[u], v)
+    }
+
+    ans := make([][]int, n)
+    for i := range n {
+        seen := make([]bool, n)
+        dfs(graph, i, i, seen, ans)
+    }
+
+    return ans
+}

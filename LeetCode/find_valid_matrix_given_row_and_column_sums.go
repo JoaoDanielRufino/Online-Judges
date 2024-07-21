2a08ea9c7d032cbc1d59832a6715056934211469
João Daniel Rufino <joaodaniel0405@gmail.com>
2024-07-21 14:11:47 -0300

    Create find_valid_matrix_given_row_and_column_sums.go

---
+++ LeetCode/find_valid_matrix_given_row_and_column_sums.go
@@ -0,0 +1,26 @@
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+
+    return b
+}
+
+func restoreMatrix(rowSum []int, colSum []int) [][]int {
+    ans := make([][]int, len(rowSum))
+
+    for i := range ans {
+        ans[i] = make([]int, len(colSum))
+    }
+    
+    for i := range rowSum {
+        for j := range colSum {
+            num := min(rowSum[i], colSum[j])
+            ans[i][j] = num
+            rowSum[i] -= num
+            colSum[j] -= num
+        }
+    }
+
+    return ans
+}

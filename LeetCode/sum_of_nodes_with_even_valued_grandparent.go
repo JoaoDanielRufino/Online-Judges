b55b98919a37036599d5c4f6a7a0a238c4a0589d
João Daniel Rufino <joaodaniel0405@gmail.com>
2024-07-25 19:44:26 -0300

    Create sum_of_nodes_with_even_valued_grandparent.go

---
+++ LeetCode/sum_of_nodes_with_even_valued_grandparent.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func solve(root, parent, grandparent *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+
+    if grandparent != nil && grandparent.Val % 2 == 0 {
+        return root.Val + solve(root.Left, root, parent) + solve(root.Right, root, parent)
+    }
+    
+    return solve(root.Left, root, parent) + solve(root.Right, root, parent)
+}
+
+func sumEvenGrandparent(root *TreeNode) int {
+    return solve(root, nil, nil)
+}
